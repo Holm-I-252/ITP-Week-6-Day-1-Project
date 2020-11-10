b89d25290bbc93643dc29489afa7c3d662ca326d
Holm-I-252 <71738701+Holm-I-252@users.noreply.github.com>
2020-11-09 19:00:06 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,83 @@
+const storeItems = [
+    {
+        name: 'TV',
+        price: 800.00,
+        inStock: true,
+        details: '4K Ultra HD'
+    },
+    {
+        name: 'Phone',
+        price: 700.00,
+        inStock: false,
+        details: '5G'
+    },
+    {
+        name: 'Game Console',
+        price: 300.00,
+        inStock: true,
+        details: 'The latest and greatest'
+    },
+    {
+        name: 'Laptop',
+        price: 1200.00,
+        inStock: true,
+        details: '16GB RAM 1TB SSD'
+    },
+    {
+        name: 'Smart Watch',
+        price: 200.00,
+        inStock: false,
+        details: 'Counts your steps'
+    },
+    {
+        name: 'Headphones',
+        price: 100.00,
+        inStock: true,
+        details: 'Clearest music to be heard'
+    },
+    {
+        name: 'Keyboard',
+        price: 100.00,
+        inStock: true,
+        details: 'Types for you'
+    },
+    {
+        name: 'HDMI Cord',
+        price: 100.00,
+        inStock: true,
+        details: 'HDMI to USB type C'
+    },
+    {
+        name: 'Monitor',
+        price: 300.00,
+        inStock: true,
+        details: '4K Ultra HD'
+    },
+    {
+        name: 'Speaker',
+        price: 200.00,
+        inStock: true,
+        details: 'Clearest music to be heard'
+    },
+    {
+        name: 'Video Game',
+        price: 60.00,
+        inStock: true,
+        details: 'Enjoy for hours'
+    },
+];
+
+for(let i = 0; i < storeItems.length; i++){
+    let names = (storeItems[i]['name'])
+    let price = (storeItems[i]['price'])
+  
+  if(storeItems[i]['inStock'] === true){
+    $('#items').append(`<div id='oneItem' class='lightMode'><p id='price' class='lightMode'>$${storeItems[i]['price']}</p><p id='name' class='lightMode'>${storeItems[i]['name']}</p> <p id='details' class='lightMode'>${storeItems[i]['details']}</p></div>`);
+  }
+}
+
+$('#button').click(function(){
+    $('.lightMode').toggleClass('darkMode');
+    $('.darkMode').toggleClass('lightMode')
+    console.log('toggle')
+})
